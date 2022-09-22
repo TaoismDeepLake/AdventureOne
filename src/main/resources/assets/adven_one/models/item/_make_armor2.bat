9b038119f4a104dc34155147dbb282248261cedf
huige <61108540+huige233@users.noreply.github.com>
2022-09-22 20:15:55 +0800

    add _make_tool.bat and _make_armor.bat add init

---
+++ src/main/resources/assets/adven_one/models/item/_make_armor2.bat
@@ -0,0 +1,59 @@
+@echo off
+:: Vazkii's JSON creator for blocks
+:: Put in your /resources/assets/%modid%/models/block
+:: Makes basic block JSON files as well as the acossiated item and simple blockstate
+:: Can make multiple blocks at once
+::
+:: Usage:
+:: _make (block name 1) (block name 2) (block name x)
+::
+:: Change this to your mod's ID
+set modid=adven_one
+
+setlocal enabledelayedexpansion
+
+for %%x in (%*) do (
+	echo Making %%x_armor0.json item
+    	(
+    		echo {
+    		echo 	"parent": "%modid%:item/handheld",
+            echo    "textures": {
+            echo    "layer0": "%modid%:items/misc/%%x_armor0",
+            echo    "layer1":"%modid%:items/overlay_2"
+            echo    }
+    		echo }
+    	) > %%x_armor0.json
+
+    	echo Making %%x_armor1.json item
+        (
+        	echo {
+        	echo 	"parent": "%modid%:item/handheld",
+            echo    "textures": {
+            echo    "layer0": "%modid%:items/misc/%%x_armor1",
+            echo    "layer1":"%modid%:items/overlay_2"
+            echo    }
+        		echo }
+        ) > %%x_armor1.json
+
+        echo Making %%x_armor2.json item
+        (
+            echo {
+            echo 	"parent": "%modid%:item/handheld",
+            echo    "textures": {
+            echo    "layer0": "%modid%:items/misc/%%x_armor2",
+            echo    "layer1":"%modid%:items/overlay_2"
+            echo    }
+            echo }
+        ) > %%x_armor2.json
+
+        echo Making %%x_armor3.json item
+        (
+             echo {
+             echo 	"parent": "%modid%:item/handheld",
+             echo    "textures": {
+             echo    "layer0": "%modid%:items/misc/%%x_armor3",
+             echo    "layer1":"%modid%:items/overlay_2"
+             echo    }
+             echo }
+        ) > %%x_armor3.json
+)
---
+++ src/main/resources/assets/adven_one/models/item/_make_tool.bat
@@ -0,0 +1,103 @@
+@echo off
+:: Vazkii's JSON creator for blocks
+:: Put in your /resources/assets/%modid%/models/block
+:: Makes basic block JSON files as well as the acossiated item and simple blockstate
+:: Can make multiple blocks at once
+::
+:: Usage:
+:: _make (block name 1) (block name 2) (block name x)
+::
+:: Change this to your mod's ID
+set modid=adven_one
+
+setlocal enabledelayedexpansion
+
+for %%x in (%*) do (
+	echo Making %%x_1_pickaxe.json item
+	(
+		echo {
+		echo 	"parent": "%modid%:item/handheld",
+        echo    "textures": {
+        echo    "layer0": "%modid%:items/misc/%%x_1_pickaxe"
+        echo    "layer1":"%modid%:items/overlay_1"
+        echo    }
+		echo }
+	) > %%x_1_pickaxe.json
+
+	echo Making %%x_1_sword.json item
+    	(
+    		echo {
+    		echo 	"parent": "%modid%:item/handheld",
+            echo    "textures": {
+            echo    "layer0": "%modid%:items/misc/%%x_1_sword"
+            echo    "layer1":"%modid%:items/overlay_1"
+            echo    }
+    		echo }
+    	) > %%x_1_sword.json
+
+    echo Making %%x_2_pickaxe.json item
+    	(
+    		echo {
+    		echo 	"parent": "%modid%:item/handheld",
+            echo    "textures": {
+            echo    "layer0": "%modid%:items/misc/%%x_2_pickaxe"
+            echo    "layer1":"%modid%:items/overlay_2"
+            echo    }
+    		echo }
+    	) > %%x_2_pickaxe.json
+
+    	echo Making %%x_2_sword.json item
+        	(
+        		echo {
+        		echo 	"parent": "%modid%:item/handheld",
+                echo    "textures": {
+                echo    "layer0": "%modid%:items/misc/%%x_2_sword"
+                echo    "layer1":"%modid%:items/overlay_2"
+                echo    }
+        		echo }
+        	) > %%x_2_sword.json
+
+    echo Making %%x_3_pickaxe.json item
+    	(
+    		echo {
+    		echo 	"parent": "%modid%:item/handheld",
+            echo    "textures": {
+            echo    "layer0": "%modid%:items/misc/%%x_3_pickaxe"
+            echo    "layer1":"%modid%:items/overlay_3"
+            echo    }
+    		echo }
+    	) > %%x_3_pickaxe.json
+
+    	echo Making %%x_3_sword.json item
+        	(
+        		echo {
+        		echo 	"parent": "%modid%:item/handheld",
+                echo    "textures": {
+                echo    "layer0": "%modid%:items/misc/%%x_3_sword"
+                echo    "layer1":"%modid%:items/overlay_3"
+                echo    }
+        		echo }
+        	) > %%x_3_sword.json
+
+    echo Making %%x_4_pickaxe.json item
+    	(
+    		echo {
+    		echo 	"parent": "%modid%:item/handheld",
+            echo    "textures": {
+            echo    "layer0": "%modid%:items/misc/%%x_4_pickaxe"
+            echo    "layer1":"%modid%:items/overlay_4"
+            echo    }
+    		echo }
+    	) > %%x_4_pickaxe.json
+
+    	echo Making %%x_4_sword.json item
+        	(
+        		echo {
+        		echo 	"parent": "%modid%:item/handheld",
+                echo    "textures": {
+                echo    "layer0": "%modid%:items/misc/%%x_4_sword"
+                echo    "layer1":"%modid%:items/overlay_4"
+                echo    }
+        		echo }
+        	) > %%x_4_sword.json
+)
