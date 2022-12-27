9b3db8788dd194bfe229687cb1295c26879b673c
huige233 <huige1463@gmail.com>
2022-12-27 19:08:35 +0800

    add _make_grass.bat

--- Initialization.bat
+++ Initialization.bat
@@ -8,6 +8,7 @@ for %%x in (%*) do (
     .\_make_fence_wood.bat %%x
     .\_make_fence_stone.bat %%x
     .\_make_log.bat %%x
+    .\_make_grass.bat %%x
     ..\item\_make.bat %%x_1_gem %%x_2_gem %%x_3_gem %%x_4_gem
     ..\item\_make_armor1.bat %%x_1 %%x_2 %%x_3 %%x_4
     ..\item\_make_armor2.bat %%x_1 %%x_2 %%x_3 %%x_4
